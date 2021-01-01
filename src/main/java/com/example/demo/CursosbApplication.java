75b6926dea27c79f47da000c5393fa080237b8dc
gabrielcampos12 <gabrielcamposfreitas12@gmail.com>
2021-01-01 19:59:42 -0300

    projeto criado

---
+++ src/main/java/com/example/demo/CursosbApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosbApplication.class, args);
+	}
+
+}
