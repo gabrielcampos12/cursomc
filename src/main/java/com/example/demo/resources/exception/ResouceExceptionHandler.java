dbc73916581df5e0616c4c381517c42878b8c92a
gabrielcampos12 <gabrielcamposfreitas12@gmail.com>
2021-01-06 16:42:30 -0300

    deletando uma categoria

--- src/main/java/com/example/demo/resources/exception/ResouceExceptionHandler.java
+++ src/main/java/com/example/demo/resources/exception/ResouceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.example.demo.services.exceptions.DateIntegrityException;
 import com.example.demo.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -15,5 +16,11 @@ public class ResouceExceptionHandler {
 	public ResponseEntity<StandardError> objectNotFoud(ObjectNotFoundException e, HttpServletRequest request){
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	@ExceptionHandler(DateIntegrityException.class)
+	public ResponseEntity<StandardError> dateIntegrity(DateIntegrityException e,HttpServletRequest request){
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+		  
 	}
 }
--- src/main/java/com/example/demo/services/CategoriaService.java
+++ src/main/java/com/example/demo/services/CategoriaService.java
@@ -3,30 +3,43 @@ package com.example.demo.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.Categoria;
 import com.example.demo.repositories.CategoriaRepository;
+import com.example.demo.services.exceptions.DateIntegrityException;
 import com.example.demo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 	@Autowired
 	CategoriaRepository repository;
-	
+
 	public Categoria find(Integer id) {
-		
+
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"
-				+ " Id: "+id+", Tipo: "+Categoria.class.getName()));
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado!" + " Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repository.save(obj);
 	}
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repository.save(obj);
 	}
-	
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DateIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
+	}
+
 }
