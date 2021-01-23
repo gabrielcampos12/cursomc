60769191f17134a14b7b05847ac1e59d2ab2a9c1
gabrielcampos12 <gabrielcamposfreitas12@gmail.com>
2021-01-22 21:34:08 -0300

    validação customizada: email não repetido no update

--- src/main/java/com/example/demo/dto/ClienteDTO.java
+++ src/main/java/com/example/demo/dto/ClienteDTO.java
@@ -8,7 +8,8 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.example.demo.domain.Cliente;
-
+import com.example.demo.services.validation.ClienteUpdate;
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	private Integer id;
